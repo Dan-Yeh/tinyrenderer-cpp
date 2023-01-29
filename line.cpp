7403872793d264e1a769181e9d5f6a57720bbb6a
Chen-Chen Yeh <ge96noj@mytum.de>
2023-01-29 23:14:12 +0800

    make line denser when it is steep

--- line.cpp
+++ line.cpp
@@ -1,14 +1,28 @@
 #include "line.h"
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
     int dx = x1 - x0;
     int dy = y1 - y0;
+
+    // draw a denser line when line is steep (dx < dy)
+    if (std::abs(dx) < std::abs(dy)) {
+        steep = true;
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        std::swap(dx, dy);
+    }
+
     float slope = dy / (float) dx;
 
     int x_increment = (x1 > x0) ? 1 : -1;
 
     for (int x=x0; x != x1; x+=x_increment) {
         int y = y0 + (x - x0) * slope;
-        image.set(x, y, color);
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
